7b7d187bf0330d79bd6cd29e12a304ab13499546
MennoBR <malburgcine@hotmail.com>
2023-04-11 23:29:37 -0300

    tcc1st

---
+++ TCC/Pong.js
@@ -0,0 +1,12 @@
+// Variáveis:
+var DIRECTION = {
+    IDLE: 0,
+    UP: 1,
+    DOWN: 2,
+    LEFT: 3,
+    RIGHT: 4
+};
+
+var rounds = [5, 5, 3, 3, 2];
+var colors = ['#labc9c', '#2ecc71', '#3498db', '#8c52ff', '#9b59b6'];
+
